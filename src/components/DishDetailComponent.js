965e70d47bf40c3b9eb2681ab2a5f365430cac06
ashwin nirmul da21-01-6311 <ashwin.metropcs@gmail.com>
2021-05-08 10:09:37 -0400

    Added log statements to debug why comments are not accessible.

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,29 +2,42 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-    constructor(props) { super(props); }
+    constructor(props) {
+        super(props);
+        // console.log(props);
+        // this.state = { currentDish: this.props.dish };
+    }
+
+    RenderDish() {
+        console.log("RenderDish:");
+        console.log(this.state);
+        return (<div></div>);
+    }
 
     render() {
-        const comments = this.props.dish.comments.map((comment) => {
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card key={comment.id}                            >
-                        <CardBody>
-                            <CardText>{comment.comment}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        });
-
-        const menu = "const menu ashwin";
+        // console.log(this.props.dish.comments);
+        // const comments = this.props.dish.comments.map((comment) => {
+        //     return (
+        //         <div className="col-12 col-md-5 m-1">
+        //             <Card key={comment.id}>
+        //                 <CardBody>
+        //                     <CardText>{comment.comment}</CardText>
+        //                 </CardBody>
+        //             </Card>
+        //         </div>
+        //     );
+        // });
+        console.log("render:");
+        console.log(this.props.dish);
+        const comments = this.RenderDish();
+        // const comments = "ashwin";
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm">
                         <Card>
                             <CardBody>
-                                <CardText>{this.props.dish.description}</CardText>
+                                {/* <CardText>{this.props.dish.description}</CardText> */}
                             </CardBody>
                         </Card>
                     </div>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,56 +1,45 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = { selectedDish: null }
     }
-
-    onDishSelect(dish) { this.setState({ selectedDish: dish }); }
 
     renderDish(dish) {
         if (dish != null)
             return (
                 <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <DishDetail dish={dish} />
-                        {/* <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText> */}
-                    </CardBody>
                 </Card>
             );
         else
             return (<div></div>);
     }
 
+    // onDishSelect(dish) { this.setState({ selectedDish: dish }); }
+
     render() {
         const menu = this.props.dishes.map(
             (dish) => {
                 return (
-                    <div className="col-12 col-md-5 m-1">
+                    <div key={dish.id} className="col-12 col-md-5 m-1">
                         <Card
-                            key={dish.id}
-                            onClick={() => this.onDishSelect(dish)}>
+                            onClick={() => this.props.onClick(dish.id)}>
                             <CardImg width="100%" src={dish.image} alt={dish.name} />
                             <CardImgOverlay>
                                 <CardTitle heading>{dish.name}</CardTitle>
                             </CardImgOverlay>
                         </Card>
-                    </div>
+                    </div >
                 );
             }
         );
 
         return (
-            <div className="container">
+            <div className="container" >
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
